be02090be2898ef0bad352636cdd0f69df0e51c5
Johnny <john@Richies-iMac.local>
2023-06-03 20:01:50 -0400

    only able to copy one link

--- api.js
+++ api.js
@@ -1,11 +1,48 @@
+import { divCreate} from "./main.js";
+
 const shortenLinkBtn = document.querySelector('.shorten-btn');
 const linkInput = document.querySelector('.text-input');
 const shortenDisplay = document.querySelector('.link-display');
 
 
+function getShortenLink(text){
+  fetch(`https://api.shrtco.de/v2/shorten?url=${text}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    const shortenLinkDiv = divCreate('shorten-div');
+    shortenLinkDiv.innerHTML = `
+      <div class='original-link'>${text}</div>
+      
+      <div class='short-link'>
+        <span class='new-link'>${data.result.full_short_link}</span>
+        <button class='copy-button'>Copy</button>
+      </div>
+    `;
 
-fetch(`https://api.shrtco.de/v2/shorten?url=${linkInput.value}`)
-.then(res => res.json())
-.then(data => {
-  console.log(data)
+    shortenDisplay.append(shortenLinkDiv);
+    linkInput.value = '';
+    
+    
+    const copyButton = document.querySelector('.copy-button');
+    const newLink = document.querySelector('.new-link');
+
+  
+    copyButton.addEventListener('click', () => {
+      navigator.clipboard.writeText(newLink.innerText);
+      copyButton.innerText = 'copied!'
+      copyButton.style.background = '#3b3054'
+    })
+  })
+  
+}
+
+shortenLinkBtn.addEventListener('click', () => {
+  if(linkInput.value === '' || linkInput.value === null){
+    alert('PLEASE ENTER A VALID URL!!!')
+  }
+  
+  getShortenLink(linkInput.value);
 })
+
+
